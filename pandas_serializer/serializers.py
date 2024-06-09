0d8a6adbc194a336afaac82aeeeb17973214e1ee
Ezra Yendau <5201040538@st.aamusted.edu.gh>
2024-06-09 00:33:28 +0000

    Added some type annotations to a few methods

--- pandas_serializer/serializers.py
+++ pandas_serializer/serializers.py
@@ -11,24 +11,26 @@ class Serializer:
 
     def __init__(self, instance=None, **kwargs):
         if self.model is None:
-            raise Exception('model must be defined')
-        self.request = kwargs.get('request')
+            raise Exception("model must be defined")
+        self.request = kwargs.get("request")
         self.instance = instance
         self.table = self.model._meta.db_table
         self.pk_field = self.model._meta.pk.name
 
     @property
-    def data(self):
+    def data(self) -> dict:
         with engine.connect() as conn:
             df = pd.read_sql_query(self.query(), conn)
-            return df.to_dict(orient='records')
+            return df.to_dict(orient="records")
 
-    def query(self):
-        fields = ', '.join(self.fields) or '*'
-        return f"SELECT {fields} FROM {self.table} WHERE {self.pk_field} EQUALS {self.instance.pk}" if self.instance else f"SELECT {fields} FROM {self.table}"
+    def query(self) -> object:
+        fields = ", ".join(self.fields) or "*"
+        return (
+            f"SELECT {fields} FROM {self.table} WHERE {self.pk_field} EQUALS {self.instance.pk}"
+            if self.instance
+            else f"SELECT {fields} FROM {self.table}"
+        )
 
     def save(self, **kwargs):
         df = pd.DataFrame(self.request.data)
-        df.to_sql(self.table, con=engine, if_exists='replace')
-
-
+        df.to_sql(self.table, con=engine, if_exists="replace")
